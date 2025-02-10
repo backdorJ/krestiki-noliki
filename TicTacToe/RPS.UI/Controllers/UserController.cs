8fe3d9aee0a06d6c76382877645baf8f541050b1
dammirka <cdbacjdorz@yandex.ru>
2025-02-11 02:55:09 +0300

    add authorize

--- TicTacToe/RPS.UI/Controllers/UserController.cs
+++ TicTacToe/RPS.UI/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using RPS.Core.Requests.User.GetUserRatings;
 using TicTacToe.MediatR;
 
@@ -6,6 +7,7 @@ namespace RPS.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
+[Authorize]
 public class UserController(IMediator mediator) : ControllerBase
 {
     [HttpGet("get-rate-users")]
